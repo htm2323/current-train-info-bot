a500453e03752455d7f4fac5a2a574bc2cfab741
htm2323 <is0578ri@ed.ritsumei.ac.jp>
2025-07-18 18:59:05 +0900

    [remove] 開発途中で使用していたViewerスクリプトを削除

--- TrainCurrentInfoViewer.py
+++
@@ -1,9 +0,0 @@
-import TrainCurrentInfoCrawler
-from TargetCompanyEnum import TargetCompany
-import time
-
-crawler = TrainCurrentInfoCrawler.TrainCurrentInfoCrawler()
-
-while True:
-    crawler.train_currentinfo_crawl(TargetCompany.JRwest)
-    time.sleep(30)
